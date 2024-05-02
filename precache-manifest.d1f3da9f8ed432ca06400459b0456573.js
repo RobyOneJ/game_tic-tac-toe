7232d8c1ca1ab894e82ad25a50ba8fa506d08b6c
roberta de cecco <roberta.dececco@gmail.com>
2024-05-02 20:13:54 +0800

    Updates

---
+++ precache-manifest.d1f3da9f8ed432ca06400459b0456573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc28abfd2a1cb94c8d9398f2348b405",
+    "url": "/game_tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ee1da860594008c60489",
+    "url": "/game_tic-tac-toe/static/css/main.4e681dd4.chunk.css"
+  },
+  {
+    "revision": "f86f7a9b94aa23be7f29",
+    "url": "/game_tic-tac-toe/static/js/2.91dc03cb.chunk.js"
+  },
+  {
+    "revision": "ee1da860594008c60489",
+    "url": "/game_tic-tac-toe/static/js/main.e631e1f8.chunk.js"
+  },
+  {
+    "revision": "aa278c16ad821ce3f2b3",
+    "url": "/game_tic-tac-toe/static/js/runtime~main.b999ea28.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game_tic-tac-toe.git/precache-manifest.b509b6f37c00463391c3f5412db27766.js"
+  "/game_tic-tac-toe/precache-manifest.d1f3da9f8ed432ca06400459b0456573.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game_tic-tac-toe.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game_tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
